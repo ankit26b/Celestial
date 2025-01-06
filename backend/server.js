7857ddc63dde85ec8c24dd3847d68d0b7bd6102c
Ankit Barmase <ankitbarmase26@gmail.com>
2025-01-06 16:14:47 +0530

    server and user setup

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import 'dotenv/config.js'
+
+import http from 'http';
+import app from './app.js'
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+
+server.listen(3000, ()=>{
+    console.log(`Server running on port ${port}`)
+})
---
+++ backend/services/user.service.js
@@ -0,0 +1,16 @@
+import userModel from "../models/user.model.js";
+
+export const createUser = async({email, password})=>{
+    if(!email || !password){
+        throw new Error('Email and password is req');
+    }
+
+    const hashedPassword = await userModel.hashPassword(password);
+
+    const user = await userModel.create({
+        email,
+        password:hashedPassword
+    });
+
+    return user;
+}
