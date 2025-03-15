97faaccc2845ef56f3a38262be281bab45b4ceb9
Ankit Barmase <ankitbarmase26@gmail.com>
2025-03-15 19:35:28 +0530

    Launch Page added

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,12 +5,14 @@ import Register from '../screens/Register'
 import Home from '../screens/Home'
 import Project from '../screens/Project'
 import UserAuth from '../auth/UserAuth'
+import Launch from '../screens/Launch'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
        <Routes>
-          <Route path="/" element = {<UserAuth><Home/></UserAuth>}/>
+          <Route path="/" element = {<Launch/>}/>
+          <Route path="/home" element = {<UserAuth><Home/></UserAuth>}/>
           <Route path="/login" element = {<Login/>}/>
           <Route path="/register" element = {<Register/>}/>
           <Route path="/project" element = {<UserAuth><Project/></UserAuth>}/>
